6f725a582250fb3e84636e53d219345afa8d6858
Oscar Stanchi <indirivacua@hotmail.com>
2025-12-18 01:44:33 -0300

    Enhance RandomMaskGenerator with upsample_mode
    Added upsample_mode parameter to RandomMaskGenerator and updated interpolation method to use it.

--- cbrise/mask_generator.py
+++ cbrise/mask_generator.py
@@ -9,13 +9,17 @@ class MaskGenerator(abc.ABC):
         pass
 
 class RandomMaskGenerator(MaskGenerator):
-    def __init__(self,batch_size:int,base_size:tuple[int],upsample_size:tuple[int]) -> None:
+    def __init__(self,batch_size:int,base_size:tuple[int],upsample_size:tuple[int],upsample_mode:str="bilinear") -> None:
         super().__init__(batch_size)
         self.base_size=base_size
         self.upsample_size=upsample_size
+		self.upsample_mode=upsample_mode
     def __next__(self,previous_mask_scores: torch.Tensor):
         base_size_channels = (self.batch_size, 1,*self.base_size)
         base_masks = torch.rand(*base_size_channels)
         base_masks = (base_masks > 0.5).float()
-        masks = torch.nn.functional.interpolate(base_masks,size=self.upsample_size,mode="bilinear",align_corners=True)
+        masks = torch.nn.functional.interpolate(base_masks,size=self.upsample_size,mode=self.upsample_mode,align_corners=True)
+        masks_min = masks.view(self.batch_size, -1).min(dim=1)[0].view(self.batch_size, 1, 1, 1)
+        masks_max = masks.view(self.batch_size, -1).max(dim=1)[0].view(self.batch_size, 1, 1, 1)
+        masks = (masks - masks_min) / (masks_max - masks_min + 1e-8)
         return masks
